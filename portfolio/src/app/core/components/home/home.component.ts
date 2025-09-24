f70a547da861f903002c715238a598fbe4a011eb
krzyspazdzior <1p22pazdzior@gmail.com>
2025-09-24 23:41:51 +0200

    new gallery projects, bug fixes

--- portfolio/src/app/core/components/home/home.component.ts
+++ portfolio/src/app/core/components/home/home.component.ts
@@ -3,10 +3,11 @@ import { HeaderComponent } from '../../shared/header/header.component';
 import { AboutMeComponent } from '../about-me/about-me.component';
 import { ClickCounterComponent } from '../click-counter/click-counter.component';
 import { LanguagesComponent } from "../languages/languages.component";
+import { GalleryComponent } from "../gallery/gallery.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, AboutMeComponent, ClickCounterComponent, LanguagesComponent],
+  imports: [HeaderComponent, AboutMeComponent, ClickCounterComponent, LanguagesComponent, GalleryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
