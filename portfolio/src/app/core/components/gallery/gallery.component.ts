778c84a1a9c57d2b10124968ffe98c043b491563
krzyspazdzior <1p22pazdzior@gmail.com>
2025-09-25 13:42:44 +0200

    browser update

--- portfolio/src/app/core/components/gallery/gallery.component.ts
+++ portfolio/src/app/core/components/gallery/gallery.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+    titles = ['Dribble Music', 'CareMatch', 'Rythmify', 'Fervo Casino', 'Rescript']; 
+    icons = ['images/dribble_icon.png', 'images/carematch_icon.png','images/rythmify_icon.png', 'images/fervo_icon.png', 'images/rescript_icon.png'];
+    imgs = ['images/dribble_pc.png', 'images/carematch_pc.png', 'images/rythmify_pc.png', 'images/fervo_pc.png', 'images/Rescript_pc.png'];
+    links = ['dribble-music.com', 'carematch.com', 'rythmify.com', 'fervo.com', 'rescript.dev'];
+    activeIndex: number = 0
 
-}
+    setActive(i:number){
+      this.activeIndex = i;
+    }
+  }
+
