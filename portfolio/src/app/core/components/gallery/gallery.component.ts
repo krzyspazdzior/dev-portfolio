78d8d4a2f43df039adfd00aef5d7a4400ebf749c
krzyspazdzior <1p22pazdzior@gmail.com>
2025-09-26 17:39:13 +0200

    browser update

--- portfolio/src/app/core/components/gallery/gallery.component.ts
+++ portfolio/src/app/core/components/gallery/gallery.component.ts
@@ -10,11 +10,40 @@ export class GalleryComponent {
     titles = ['Dribble Music', 'CareMatch', 'Rhytmify', 'Fervo Casino', 'Rescript']; 
     icons = ['images/dribble_icon.png', 'images/carematch_icon.png','images/rythmify_icon.png', 'images/fervo_icon.png', 'images/rescript_icon.png'];
     imgs = ['images/dribble_pc.png', 'images/carematch_pc.png', 'images/rythmify_pc.png', 'images/fervo_pc.png', 'images/rescript_pc.png'];
-    links = ['dribble-music.com', 'carematch.com', 'rythmify.com', 'fervo.com', 'rescript.dev'];
+    links = ['dribble-music.com', 'carematch.com', 'rhytmify.com', 'fervo.com', 'rescript.dev'];
+    hrefs = [];
     activeIndex: number = 0
 
     setActive(i:number){
       this.activeIndex = i;
+    }
+
+    refresh(){
+      const img = document.querySelector(".active-img");
+      if(img){
+        img.classList.add("img-refreshed");
+        setTimeout(() => {
+          img.classList.remove("img-refreshed");
+        }, 60)
+      }
+
+    }
+
+    menuClick() {
+      const menu = document.querySelector("#menu");
+      const dropdown = document.querySelector(".dropdown")
+      
+      if(menu && dropdown){
+            if(dropdown.classList.contains("dropdown-active")){
+              dropdown.classList.remove("dropdown-active");
+              menu.classList.remove("menu-bg");
+            }else{
+              dropdown.classList.add("dropdown-active");
+              menu.classList.add("menu-bg");
+
+            }
+            menu.classList.add("active-menu");
+        }
     }
   }
 
