e2731421fe5abc0d5fa38d04ad453ae437dfe1b2
krzyspazdzior <1p22pazdzior@gmail.com>
2025-10-22 21:00:37 +0200

    loader, minor fixes

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -4,8 +4,12 @@ import { AppComponent } from './app/app.component';
 
 
 bootstrapApplication(AppComponent, appConfig)
-
-
-
-
   .catch((err) => console.error(err));
+
+  window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  if (loader) {
+    loader.classList.add('hidden');
+    setTimeout(() => loader.remove(), 600);
+  }
+});
