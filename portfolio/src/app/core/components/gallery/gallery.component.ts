e8c9190f2ac3ac4e607de8e0c88bd3b26c5aacd6
krzyspazdzior <1p22pazdzior@gmail.com>
2025-10-31 11:08:14 +0100

    minor fixes

--- portfolio/src/app/core/components/gallery/gallery.component.ts
+++ portfolio/src/app/core/components/gallery/gallery.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslatePipe } from '@ngx-translate/core';
-
 @Component({
   standalone: true,
   selector: 'app-gallery',
